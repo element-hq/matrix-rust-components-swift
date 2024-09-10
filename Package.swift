50f8730000a1cc9ebe75ecf879c2b85b36adfd34
Stefan Ceriu <stefan.ceriu@gmail.com>
2024-09-10 16:10:45 +0300

    Bump to version v1.0.46 (matrix-rust-sdk/main a6f84d8513cb5008c07225280f93f5f6919a188a)

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
-let checksum = "e97e6382029f9a2ebcf883b5534edbb6c6d2586e3f7ff2c61a7838b7cf2a994b"
-let version = "v1.0.45"
+let checksum = "ee1bff04c2753d4e78e3a2ee376908519c021b07dd19d40ccd62be2c55ce242d"
+let version = "v1.0.46"
 let url = "https://github.com/element-hq/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 let package = Package(
     name: "MatrixRustSDK",
