cdd43015b75d5f7904b8c7d1697bd53afeedeb35
Stefan Ceriu <stefan.ceriu@gmail.com>
2025-05-02 14:06:29 +0300

    Bump to version 0.0.3-element-call (matrix-rust-sdk/toger5/widget-to-device-with-encryption 35501dfe8a8a61dcc5c7490bce24ad4101a7f25e)

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
-let checksum = "fff02d473b6639128e84018abc0358c9403d88b2cfc0772dc0cecb3d77a714ac"
-let version = "0.0.2-element-call"
+let checksum = "9bbd5175bd39b3d5a4878db9bf4516dd00ac180c8c9972655b0e08fa2b7739ac"
+let version = "0.0.3-element-call"
 let url = "https://github.com/element-hq/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 let package = Package(
     name: "MatrixRustSDK",
