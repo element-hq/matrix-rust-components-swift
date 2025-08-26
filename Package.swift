c87560bd157c83d63c3414f6c1fe1dc3a3b7d515
Doug <douglase@element.io>
2025-08-26 16:09:23 +0100

    Bump to version 25.08.26 (matrix-rust-sdk/main f4ce4356ab674ad33177fa5548813d9638dbe4cb)

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
-let checksum = "333c02562a44f4a278deeec044044c680f639b50fd2c72fccef61d82fff8d6d0"
-let version = "25.08.15"
+let checksum = "d583e5554ebd2928a0d0c602b227a0054741b80ab6e781240e662aa4fea40193"
+let version = "25.08.26"
 let url = "https://github.com/element-hq/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 let package = Package(
     name: "MatrixRustSDK",
